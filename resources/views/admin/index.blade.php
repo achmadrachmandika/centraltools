c3e9acec3317e92e8b1493df6c7c585f1022f126
achmadrachmandika <119827535+achmadrachmandika@users.noreply.github.com>
2024-05-06 16:17:00 +0700

    done dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,6 +19,15 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <style>
+        .card:hover {
+            cursor: pointer;
+            transform: scale(1.05);
+            /* Atau animasi lain sesuai kebutuhan */
+            transition: transform 0.3s ease;
+            /* Atur durasi dan jenis animasi yang diinginkan */
+        }
+    </style>
 
 </head>
 
@@ -214,12 +223,12 @@
                 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2 position-relative">
+                            <div class="card border-left-success shadow h-100 py-2 position-relative">
                                 <a href="{{ route('spm.index')}}">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
-                                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                     TOTAL SPM
                                                 </div>
                                                 <div style="display:flex">
@@ -267,12 +276,12 @@
                 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                           <div class="card border-left-primary shadow h-100 py-2 position-relative">
+                           <div class="card border-left-warning shadow h-100 py-2 position-relative">
                                 <a href="{{ route('bom.index')}}">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
-                                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                                     TOTAL BOM
                                                 </div>
                                                 <div style="display:flex">
@@ -296,7 +305,7 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 TOTAL MATERIAL
                                             </div>
                                             <div style="display:flex">
@@ -319,7 +328,7 @@
                         </div>
                 
                         <div class="col-xl-3 col-md-6 mb-4">
-                           <div class="card border-left-success shadow h-100 py-2 position-relative">
+                           <div class="card border-left-primary shadow h-100 py-2 position-relative">
                             <a href="{{ route('bpm.index')}}">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
