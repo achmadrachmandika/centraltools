b85e6f72054c0f1a5c136bebf702368b621a80e1
satriiadaffa <satriiadaffa@gmail.com>
2024-06-26 10:25:16 +0700

    done update role

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -221,10 +221,11 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="{{asset('img/undraw_profile.svg')}}">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                @if(Auth::user()->hasRole('admin'))
                                 <a href="{{route('trash')}}" style="text-decoration: none">
                                     <button class="dropdown-item" style="cursor:pointer">
                                         <i class="fas fa-trash fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -237,6 +238,7 @@
                                         Log Aktifitas
                                     </button>
                                 </a> 
+                                @endif
                                 <form  action="{{ route('logout') }}" method="POST">
                                     @csrf
                                     <button class="dropdown-item" style="cursor:pointer">
