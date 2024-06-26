039d29226f138cd07a95724d55bc59a7eeb91549
achmadrachmandika <119827535+achmadrachmandika@users.noreply.github.com>
2024-06-26 10:44:18 +0700

    sticky header dan search pada log act dan delete

--- app/Http/Controllers/SpmController.php
+++ app/Http/Controllers/SpmController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Spm;
 use App\Models\Material;
 use App\Models\project;
-use App\Models\Notification;
+use App\Models\notification;
 
 use Illuminate\Support\Facades\Bus;
 
